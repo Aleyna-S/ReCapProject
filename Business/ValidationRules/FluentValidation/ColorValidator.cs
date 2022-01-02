81e0c7e7ac7a88ded93e26cc4a7574123a2449fc
Aleyna Sahin <as.aleyna.sahin96@gmail.com>
2022-01-02 16:37:21 +0300

    12.GünÖdev1-2-3

--- Business/ValidationRules/FluentValidation/ColorValidator.cs
+++ Business/ValidationRules/FluentValidation/ColorValidator.cs
@@ -1,7 +1,7 @@
 ﻿using FluentValidation;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
+using Entities.Concrete;
 using System.Text;
 
 namespace Business.ValidationRules.FluentValidation
@@ -10,8 +10,8 @@ namespace Business.ValidationRules.FluentValidation
     {
         public ColorValidator()
         {
-            RuleFor(c => c.Name).MinimumLength(2);
             RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.Name).MinimumLength(2);
         }
     }
 }
